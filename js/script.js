807276a874465cc39bffcb3d423a557b7ee02c92
Racine Fellipe <racine.fell@gmail.com>
2023-03-24 20:03:26 -0300

    criando seleção de eventos e evento de validação e save do form

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// seleção de elementos
+const todoForm = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector("#todo-list");
+const editForm = document.querySelector("#edit-form");
+const editInput = document.querySelector("#edit-input");
+const cancelEditBtn = document.querySelector("#cancel-edit-btn");
+
+//Funçôes
+
+
+//Eventos
+todoForm.addEventListener("submit", (e) =>{
+    e.preventDefault();
+
+    const inputValue = todoInput.value
+
+    if(inputValue){
+        console.log(inputValue);
+        //save todo
+    }
+})

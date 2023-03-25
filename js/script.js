fcb0c8a5ab3663adaf73288e5d4899d3157106f8
Racine Fellipe <racine.fell@gmail.com>
2023-03-25 12:57:04 -0300

    criando funções para adicionar as tarefas

--- js/script.js
+++ js/script.js
@@ -7,7 +7,35 @@ const editInput = document.querySelector("#edit-input");
 const cancelEditBtn = document.querySelector("#cancel-edit-btn");
 
 //Funçôes
+const saveTodo = (texto) =>{
 
+    const todo = document.createElement("div");
+    todo.classList.add("todo");
+
+    const todoTitle = document.createElement("h3");
+    todoTitle.innerText = texto;
+    todo.appendChild(todoTitle);
+
+    const doneBtn = document.createElement("button");
+    doneBtn.classList.add("finish-todo");
+    doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>'
+    todo.appendChild(doneBtn);
+
+    const editBtn = document.createElement("button");
+    editBtn.classList.add("edit-todo");
+    editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>'
+    todo.appendChild(editBtn);
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("remove-todo");
+    deleteBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>'
+    todo.appendChild(deleteBtn);
+
+    todoList.appendChild(todo);
+
+    todoInput.value = "";
+    todoInput.focus();
+}
 
 //Eventos
 todoForm.addEventListener("submit", (e) =>{
@@ -16,7 +44,8 @@ todoForm.addEventListener("submit", (e) =>{
     const inputValue = todoInput.value
 
     if(inputValue){
-        console.log(inputValue);
         //save todo
+        saveTodo(inputValue)
+        
     }
 })
